feb6562b0defda4a647c008ac9aa134bacf405e5
sergmiller <sergey.miller@phystech.edu>
2015-10-27 01:48:40 +0300

    fix running testGetNameOfCurrentLocation with PowerMockito

--- sergmiller/src/test/java/ru/fizteh/fivt/students/sergmiller/moduleTests/library/GeoLocationResolverTest.java
+++ sergmiller/src/test/java/ru/fizteh/fivt/students/sergmiller/moduleTests/library/GeoLocationResolverTest.java
@@ -1,31 +1,36 @@
-//package ru.fizteh.fivt.students.sergmiller.moduleTests.library;
+package ru.fizteh.fivt.students.sergmiller.moduleTests.library;
 
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import ru.fizteh.fivt.students.sergmiller.twitterStream.GeoLocationResolver;
+//import ru.fizteh.fivt.students.sergmiller.twitterStream.exceptions.GettingMyLocationException;
+
+//import twitter4j.GeoLocation;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+
 /**
  * Created by sergmiller on 26.10.15.
  */
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({URL.class, GeoLocationResolver.class})
+@PrepareForTest({URL.class})
 public class GeoLocationResolverTest extends TestCase {
     //private URL GeoIPUrl;
     private URL dummyURL;
-    private final String URLAdress = "http://ipinfo.io/json";
-    private final String DOLGOPRUDNYY = "Dolgoprudnyy";
-    private final String resultLocationFromMyIPInDolgoprudnyy = "{"
+    static final String URLAdress = "http://ipinfo.io/json";
+    static final String DOLGOPRUDNYY = "Dolgoprudnyy";
+    String resultLocationFromMyIPInDolgoprudnyy = "{"
             + "\"country\":\"RU\","
             + "\"loc\":\"55.9041,37.5606\","
             + "\"hostname\":\"No Hostname\","
@@ -38,10 +43,6 @@ public class GeoLocationResolverTest extends TestCase {
 
     @Before
     public void preparationForTest() throws Exception {
-        //GeoIPUrl = PowerMockito.mock(URL.class);
-        //PowerMockito.whenNew(URL.class).withArguments("http://maps.googleapis.com/maps/api/geocode/json?address="
-        // + MOSCOW +"&sensor=false").thenReturn(GeoIPUrl);
-
         dummyURL = PowerMockito.mock(URL.class);
         PowerMockito.whenNew(URL.class).withArguments(URLAdress).thenReturn(dummyURL);
     }
@@ -51,7 +52,8 @@ public class GeoLocationResolverTest extends TestCase {
         InputStream inputStream =
                 new ByteArrayInputStream(resultLocationFromMyIPInDolgoprudnyy.getBytes(StandardCharsets.UTF_8));
         PowerMockito.when(dummyURL.openStream()).thenReturn(inputStream);
-        String location = new GeoLocationResolver().getNameOfCurrentLocation();
+        GeoLocationResolver geoLocationResolver = new GeoLocationResolver();
+        String location = geoLocationResolver.getNameOfCurrentLocation();
         assertEquals(location, DOLGOPRUDNYY);
     }
 }
