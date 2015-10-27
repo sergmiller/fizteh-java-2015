52c2bd34fe9ee866414245d4502c050e89ad2bd9
sergmiller <sergey.miller@phystech.edu>
2015-10-28 01:10:01 +0300

    one more fix

--- sergmiller/src/main/java/ru/fizteh/fivt/students/sergmiller/twitterStream/GeoLocationResolver.java
+++ sergmiller/src/main/java/ru/fizteh/fivt/students/sergmiller/twitterStream/GeoLocationResolver.java
@@ -130,9 +130,9 @@ public class GeoLocationResolver {
                 JSONObject locationInfo =
                         new JSONObject(responseBuilder.toString());
 
-//                System.out.print("************************************\n"
-//                + locationInfo.toString()
-//                + "\n************************************\n");
+                System.out.print("************************************\n"
+                + locationInfo.toString()
+                + "\n************************************\n");
 
                 return locationInfo.getString("city");
             } catch (IOException | JSONException e) {
