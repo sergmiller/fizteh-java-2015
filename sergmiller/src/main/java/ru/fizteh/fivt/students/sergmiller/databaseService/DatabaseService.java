d9e897826f2da8e98becfbe98f824fd43a16e840
sergmiller <sergey.miller@phystech.edu>
2015-12-16 19:40:20 +0300

    add drop, insert and getAll by all sql types

--- sergmiller/src/main/java/ru/fizteh/fivt/students/sergmiller/databaseService/DatabaseService.java
+++ sergmiller/src/main/java/ru/fizteh/fivt/students/sergmiller/databaseService/DatabaseService.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static java.lang.annotation.ElementType.FIELD;
@@ -16,15 +17,16 @@ import static java.util.stream.Collectors.joining;
  * Created by sergmiller on 15.12.15.
  */
 
-
-public class DatabaseService<T> implements Cloneable {
+@SuppressWarnings("Duplicates")
+public class DatabaseService<T> {
 
     private Class<T> workingClass;
     private Table annotation;
     private String table;
     private Field[] fields;
-//    private List<String> columns;
-//    private List<Object> values;
+
+    private static final String DATABASE_COONECTION_PATH = "jdbc:h2:./database/miniorm";
+
 
     @Target(TYPE)
     @Retention(RUNTIME)
@@ -38,6 +40,12 @@ public class DatabaseService<T> implements Cloneable {
         String name();
     }
 
+    @Target(FIELD)
+    @Retention(RUNTIME)
+    public @interface PrimaryKey {
+    }
+
+
     public DatabaseService(Class<T> workingClass) {
         this.workingClass = workingClass;
         annotation = workingClass.getAnnotation(Table.class);
@@ -45,37 +53,33 @@ public class DatabaseService<T> implements Cloneable {
         fields = workingClass.getDeclaredFields();
     }
 
-    public final void createTable() {
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:/tmp/lesson1")) {
-
-            Statement statement = connection.createStatement();
-            StringBuilder creationRequest = new StringBuilder();
-            creationRequest.append("CREATE TABLE IF NOT EXISTS " + table + " (");
-
-            int i = 0;
-
-            for (Field field : fields) {
-                Column column = field.getAnnotation(Column.class);
-                if (column != null) {
-                    if (i != 0) {
-                        creationRequest.append(", ");
-                    }
-                    creationRequest.append(column.name() + " VARCHAR(255)");
-                    ++i;
+    public void createTable() throws SQLException {
+        StringBuilder headBuilder = new StringBuilder();
+        int i = 0;
+        for (Field field : fields) {
+            if (i == 0) {
+                ++i;
+            } else {
+                headBuilder.append(", ");
+            }
+            Column column = field.getAnnotation(Column.class);
+            if (column != null) {
+                headBuilder.append(column.name()).append(" ")
+                        .append(H2TypeResolver.resolveType(field.getType()));
+                if (field.isAnnotationPresent(PrimaryKey.class)) {
+                    headBuilder.append(" NOT NULL PRIMARY KEY");
                 }
             }
+        }
 
-
-            creationRequest.append(" )");
-
-            statement.execute(creationRequest.toString());
-        } catch (SQLException e) {
-            e.printStackTrace();
+        try (Connection connection = DriverManager.getConnection(DATABASE_COONECTION_PATH)) {
+            connection.createStatement().execute("CREATE TABLE IF NOT EXISTS " + table
+                    + "(" + headBuilder.toString() + ")");
         }
     }
 
-    public final <T> void insert(T note) {
-        if (note.getClass() != workingClass) {
+    public <T> void insert(T entity) throws SQLException, IllegalAccessException {
+        if (entity.getClass() != workingClass) {
             System.err.println("wrong object");
             return;
         }
@@ -84,74 +88,65 @@ public class DatabaseService<T> implements Cloneable {
         List<Object> values = new ArrayList<>();
 
 
-        try {
-            for (Field field : fields) {
-                field.setAccessible(true);
-                Column column = field.getAnnotation(Column.class);
-                if (column != null) {
-                    columns.add(column.name());
-                    values.add(field.get(note));
-                }
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Column column = field.getAnnotation(Column.class);
+            if (column != null) {
+                columns.add(column.name());
+                values.add(field.get(entity));
             }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
         }
 
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:/tmp/lesson1")) {
-            String columnsLine = columns.stream().collect(joining(", "));
-            String valuesLine = values.stream()
-                    .map(o -> "'" + o.toString() + "'")
-                    .collect(joining(", "));
+        String columnsLine = columns.stream().collect(joining(", "));
+        String valuesLine = values.stream()
+                .map(x -> " ? ")
+                .collect(joining(", "));
 
-            PreparedStatement statement =
-                    connection.prepareStatement(
-                            "INSERT INTO " + table + " (" + columnsLine + ") "
-                                    + "VALUES (" + valuesLine + ")");
-            int updatedLines = statement.executeUpdate();
+        StringBuilder requestBuilder = new StringBuilder();
+        requestBuilder.append("INSERT INTO ")
+                .append(table)
+                .append(" (").append(columnsLine).append(") ")
+                .append("VALUES (").append(valuesLine).append(")");
 
-            System.out.println("Updated: " + updatedLines);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        try (Connection connection = DriverManager.getConnection(DATABASE_COONECTION_PATH)) {
+            PreparedStatement insertStatement
+                    = connection.prepareStatement(requestBuilder.toString());
+            for (int i = 0; i < values.size(); ++i) {
+                insertStatement.setObject(i + 1, values.get(i));
+            }
+            insertStatement.execute();
         }
     }
 
-    public final List<T> queryForAll() {
-        List<T> answer = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:/tmp/lesson1")) {
-            try (ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM " + table)) {
-                while (rs.next()) {
-
-                    T instance = workingClass.newInstance();
-                    //copy-paste
-//                    for (int i = 0; i < fields.length; ++i) {
-//                        if (fields[i].getClass().isAssignableFrom(Number.class)) {
-//                            Long val = rs.getLong(i + 1);
-//                            fields[i].set(instance, val);
-//                        } else if (fields[i].getType() != String.class) {
-//                            fields[i].set(instance, rs.getObject(i + 1));
-//                        } else {
-//                            Clob instance = rs.getClob(i + 1);
-//                            fields[i].set(instance,
-//                                    instance.getSubString(1, (int) instance.length()));
-//                        }
-//                    }
-                    for (Field field : fields) {
-                        Column column = field.getAnnotation(Column.class);
-                        if (column == null) {
-                            continue;
-                        }
-                        field.set(instance, rs.getObject(column.name()));
-                    }
-                    answer.add(instance);
-                }
-            }
-
-
-        } catch (SQLException | InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
+    public List<T> queryForAll() throws SQLException, InstantiationException, IllegalAccessException {
+        try (Connection connection = DriverManager.getConnection(DATABASE_COONECTION_PATH)) {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + table);
+            ResultSet rs = statement.executeQuery();
+            return buildRequetAnswer(rs);
         }
+    }
 
+    public void dropTable() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(DATABASE_COONECTION_PATH)) {
+            connection.createStatement().execute("DROP TABLE IF EXISTS " + table);
+        }
+    }
+
+    private <T> List<T> buildRequetAnswer(final ResultSet rs) throws SQLException,
+            IllegalAccessException, InstantiationException {
+        List<T> answer = new LinkedList<>();
+        while (rs.next()) {
+            T record = (T) workingClass.newInstance();
+            for (Field field : fields) {
+                //field.setAccessible(true);
+                Column column = field.getAnnotation(Column.class);
+                if (column == null) {
+                    continue;
+                }
+                field.set(record, rs.getObject(column.name()));
+            }
+            answer.add(record);
+        }
         return answer;
     }
 }
--- sergmiller/src/test/java/ru/fizteh/fivt/students/sergmiller/databaseServiceTests/DatabaseServiceTests.java
+++ sergmiller/src/test/java/ru/fizteh/fivt/students/sergmiller/databaseServiceTests/DatabaseServiceTests.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 import ru.fizteh.fivt.students.sergmiller.databaseService.DatabaseService;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,18 +14,22 @@ import java.util.List;
 public class DatabaseServiceTests extends TestCase {
     @DatabaseService.Table(name = "users")
     public static class User {
+        public User() {
+        }
 
+        ;
         @DatabaseService.Column(name = "name")
-        String name;
+        @DatabaseService.PrimaryKey
+        public String name;
 
         @DatabaseService.Column(name = "age")
-        String age;
+        public int age;
 
         public void setName(String name) {
             this.name = name;
         }
 
-        public void setAge(String age) {
+        public void setAge(int age) {
             this.age = age;
         }
 
@@ -39,25 +44,43 @@ public class DatabaseServiceTests extends TestCase {
 
     @Test
     public void testInitTable() throws Exception {
-        DatabaseService databaseService = new DatabaseService(User.class);
+        DatabaseService<User> databaseService = new DatabaseService(User.class);
+        databaseService.dropTable();
         databaseService.createTable();
-        User user = new User();
-        user.setName("Alice");
-        user.setAge("19");
+        User user1 = new User();
+        user1.setName("Bob");
+        user1.setAge(21);
 
-        databaseService.insert(user);
+        databaseService.insert(user1);
 
-        user.setName("Bob");
-        user.setAge("21");
+        User user2 = new User();
+        user2.setName("Alice");
+        user2.setAge(19);
 
-        databaseService.insert(user);
+        databaseService.insert(user2);
 
-        List users = databaseService.queryForAll();
+        List<User> users = databaseService.queryForAll();
 
-        Iterator<User> it = users.iterator();
+        //assertEquals(user1, users.get(0));
 
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
+        assertEquals("User{name='Bob', age=21}", users.get(0).toString());
+        assertEquals("User{name='Alice', age=19}", users.get(1).toString());
+        assertEquals(2, users.size());
+
+        databaseService.dropTable();
+        databaseService.createTable();
+
+        User user3 = new User();
+        user3.setName("Serg");
+        user3.setAge(19);
+
+        databaseService.insert(user3);
+
+        users = databaseService.queryForAll();
+
+        assertEquals("User{name='Serg', age=19}", users.get(0).toString());
+        assertEquals(1, users.size());
+
+        databaseService.dropTable();
     }
 }
